e0a1634ecfecc2d650d5a66296cb6dcec019cb91
Markus Schmall <markus@schmall.io>
2025-10-06 17:30:20 +0200

    Fixes

--- src/backendng/bin/main/com/secman/security/SecurityConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.secman.security
-
-import io.micronaut.context.annotation.Factory
-
-@Factory
-class SecurityConfig {
-    // JWT configuration is now handled entirely through application.yml
-    // This removes potential bean creation conflicts
-}
