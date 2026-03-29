7ca4e9ae784816ff0cc3f336e90b0a7da7fe530d
Markus Schmall <markus@schmall.io>
2026-03-29 17:17:07 +0200

    Enable SECMAN_DEBUG and clean workspace changelist
    Set SECMAN_DEBUG=true in bin/backenddev.sh to enable debug in the dev environment. Update .idea/workspace.xml changelist by removing stale entries (workspace.xml and bin/secmanng), leaving the backenddev.sh change.

--- bin/backenddev.sh
+++ bin/backenddev.sh
@@ -4,6 +4,7 @@
 export MICRONAUT_ENVIRONMENTS=dev
 export SECMAN_BACKEND_URL="op://test/secman/SECMAN_BACKEND_BASE_URL"
 export DB_CONNECT="op://test/secman/DB_CONNECT"
+export SECMAN_DEBUG=true
 
 export JWT_SECRET=$(openssl rand -base64 48)
 
