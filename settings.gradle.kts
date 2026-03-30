605a2dccce04a9a4d1fd7e6a95806941edaa0373
Markus Schmall <markus@schmall.io>
2026-03-30 21:02:29 +0200

    Bump Kotlin and shadow plugin versions
    Update Kotlin Gradle plugin IDs from 2.3.10 to 2.3.20 and bump com.gradleup.shadow from 9.3.2 to 9.4.0 in settings.gradle.kts. Also adjust .idea/workspace.xml changelist to reference settings.gradle.kts and remove stale entries (IDE metadata-only change).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,14 @@ pluginManagement {
         mavenCentral()
     }
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "2.3.10"
-        id("org.jetbrains.kotlin.plugin.allopen") version "2.3.10"
-        id("org.jetbrains.kotlin.plugin.jpa") version "2.3.10"
+        id("org.jetbrains.kotlin.jvm") version "2.3.20"
+        id("org.jetbrains.kotlin.plugin.allopen") version "2.3.20"
+        id("org.jetbrains.kotlin.plugin.jpa") version "2.3.20"
         id("com.google.devtools.ksp") version "2.3.6"
         id("io.micronaut.application") version "4.6.2"
         id("io.micronaut.library") version "4.6.2"
         id("io.micronaut.aot") version "4.6.2"
-        id("com.gradleup.shadow") version "9.3.2"
+        id("com.gradleup.shadow") version "9.4.0"
     }
 }
 
