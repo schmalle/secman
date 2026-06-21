f1325215b79fcb0b78d0496cc0535be735d48e29
Markus Schmall <markus@schmall.io>
2026-06-21 21:23:00 +0200

    Fixed URLs

--- scripts/startfrontenddev.sh
+++ scripts/startfrontenddev.sh
@@ -4,7 +4,8 @@
 cd src/frontend
 export SECMAN_DOMAIN="pass://Test/SECMAN/SECMAN_BACKEND_BASE_URL"
 export SECMAN_HOST="pass://Test/SECMAN/SECMAN_HOST"
+export FRONTEND_URL="pass://Test/SECMAN/SECMAN_BACKEND_BASE_URL"
+export SECMAN_BACKEND_URL="pass://Test/SECMAN/SECMAN_BACKEND_BASE_URL"
+
 
 pass-cli run -- npm run dev
-
-
